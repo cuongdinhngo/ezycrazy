ef4be726191acdc1e1a80a7f682a94b22d6ba139
AT-Cuong.Ngo <cuong.ngo@asiantech.vn>
2020-03-19 10:03:10 +0700

    deleted unneccessary files

--- project/app/Controllers/Exception/ControllerException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Atom\Controllers\Exception;
-
-class ControllerException extends \Exception
-{
-    const ERR_MSG_INVALID_CONTROLLER = "Controller Not Found";
-    const ERR_MSG_ACTION_FAIL = "Action Failed";
-}
