a3e14aab65e84d8cee1fd540bb1358cd5f3bef89
Ngo Dinh Cuong <cuongnd@nitrotech.asia>
2020-08-03 20:42:16 +0700

    CI: fixed phpcs

--- app/dbal_bootstrap.php
+++ app/dbal_bootstrap.php
@@ -7,16 +7,16 @@ use Doctrine\ORM\EntityManager;
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $config = Setup::createAnnotationMetadataConfiguration(
-	array(DOC_ROOT.env('DBAL_PATH_CONFIG')), 
-	(bool) env('DBAL_DEV_MODE'), 
-	env('DBAL_PROXY_DIR') ? env('DBAL_PROXY_DIR') : null, 
-	env('DBAL_CACHE') ? env('DBAL_CACHE') : null, 
-	(bool) env('DBAL_USE_SIMPLE_ANNO_READER')
+    array(DOC_ROOT.env('DBAL_PATH_CONFIG')),
+    (bool) env('DBAL_DEV_MODE'),
+    env('DBAL_PROXY_DIR') ? env('DBAL_PROXY_DIR') : null,
+    env('DBAL_CACHE') ? env('DBAL_CACHE') : null,
+    (bool) env('DBAL_USE_SIMPLE_ANNO_READER')
 );
 
 // database configuration parameters
 $conn = array(
-	'driver' => env('DB_DRIVER'),
+    'driver' => env('DB_DRIVER'),
     'dbname' => env('DB_NAME'),
     'user' => env('DB_USER'),
     'password' => env('DB_PASSWORD'),
